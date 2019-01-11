bac1d46a7f4a4adce3a29ab12c3a8451d427c377
jasonliu119 <weijiejason119@gmail.com>
2019-01-11 15:35:48 -0800

    add the watchdog for the docker render

---
+++ my_oddsportal/run_timed_check_program.py
@@ -0,0 +1,39 @@
+import os
+import time
+import subprocess
+
+GREP_FILE = 'grep.txt'
+
+# check whether a program is running by looking at the "ps -aux | grep <some str>"
+def check_is_running(grep_param, want_str):
+    os.system("ps -a | grep \"{}\" > {}".format(grep_param, GREP_FILE))
+    content = ''
+    with open(GREP_FILE) as f:
+        content = f.read()
+    print 'Got content from grep:\n' + content
+    return (want_str in content)
+
+def loop_and_check(cmd, grep_param, want_str):
+    while True:
+        subprocess.Popen(cmd, shell=True)
+        print "Started: " + cmd
+        time.sleep(10)
+
+        while True:
+            try:
+                is_running = check_is_running(grep_param, want_str)
+                if not is_running:
+                    break
+                else:
+                    print "Found " + want_str + " from grep"
+            except Exception as e:
+                print "Exception in loop_and_check: " + e.message
+                break
+
+            time.sleep(3 * 60)
+
+if __name__ == "__main__":
+    cmd = 'sudo docker run -p 8050:8050 scrapinghub/splash'
+    grep_param = 'docker run'
+    want_str = 'sudo docker run -p'
+    loop_and_check(cmd, grep_param, want_str) 
